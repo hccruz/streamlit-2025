84d0b933cf6422c744d664478b72676f5c2d1ad1
Heraldo Cruz <hccruz@gmail.com>
2025-07-18 23:18:46 -0300

    'feat: Adicionado Estatísticas Gerais'

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
 import streamlit as st
 import pandas as pd
 
+def calc_general_stats(df:pd.DataFrame):
+    df_data = df.groupby(by='Data')[['Valor']].sum()
+    df_data['lag_1'] = df_data['Valor'].shift(1)
+    df_data['Diferença Mensal Abs.'] = df_data['Valor'] - df_data['lag_1']
+    df_data['Média 6M Diferença Mensal Abs.']= df_data['Diferença Mensal Abs.'].rolling(6).mean()
+    df_data['Média 12M Diferença Mensal Abs.'] = df_data['Diferença Mensal Abs.'].rolling(12).mean()
+    df_data['Média 24M Diferença Mensal Abs.'] = df_data['Diferença Mensal Abs.'].rolling(24).mean()
+    
+    df_data['Diferença Mensal Rel.'] = df_data['Valor'] / df_data['lag_1'] - 1
+    
+    df_data['Evolução 6M Total'] = df_data['Valor'].rolling(6).apply(lambda x: x[-1] - x[0])
+    df_data['Evolução 12M Total'] = df_data['Valor'].rolling(12).apply(lambda x: x[-1] - x[0])
+    df_data['Evolução 24M Total'] = df_data['Valor'].rolling(24).apply(lambda x: x[-1] - x[0])
+    
+    df_data['Evolução 6M Relativa'] = df_data['Valor'].rolling(6).apply(lambda x: x[-1] / x[0] - 1)
+    df_data['Evolução 12M Relativa'] = df_data['Valor'].rolling(12).apply(lambda x: x[-1] / x[0] - 1)
+    df_data['Evolução 24M Relativa'] = df_data['Valor'].rolling(24).apply(lambda x: x[-1] / x[0] - 1)
+    
+    df_data = df_data.drop('lag_1', axis=1)
+    
+    return df_data
+
 st.set_page_config(page_title='Finanças', page_icon='💰')
 
 st.markdown('''
@@ -50,5 +72,48 @@ if file_upload:
         
         # Gráfico de distribuição
         st.bar_chart(df_instituicao.loc[date])
+    
+    exp3 = st.expander('Estatísticas Gerais')
+    tab_stats, tab_abs, tab_rel = exp3.tabs(tabs=['Estatísticas', 'Histórico de Evolução', 'Crescimento Relativo'])
+    
+    df_stats = calc_general_stats(df)    
+    columns_config = {
+        'Valor': st.column_config.NumberColumn('Valor', format='R$ %.2f'),
+        'Diferença Mensal Abs.': st.column_config.NumberColumn('Diferença Mensal Abs.', format='R$ %.2f'),
+        'Média 6M Diferença Mensal Abs.': st.column_config.NumberColumn('Média 6M Diferença Mensal Abs.', format='R$ %.2f'),
+        'Média 12M Diferença Mensal Abs.': st.column_config.NumberColumn('Média 12M Diferença Mensal Abs.', format='R$ %.2f'),
+        'Média 24M Diferença Mensal Abs.': st.column_config.NumberColumn('Média 24M Diferença Mensal Abs.', format='R$ %.2f'),
+        'Diferença Mensal Rel.':st.column_config.NumberColumn('Diferença Mensal Rel.', format='percent'),
+        'Evolução 6M Total': st.column_config.NumberColumn('Evolução 6M Total', format='R$ %.2f'),
+        'Evolução 12M Total': st.column_config.NumberColumn('Evolução 12M Total', format='R$ %.2f'),
+        'Evolução 24M Total': st.column_config.NumberColumn('Evolução 24M Total', format='R$ %.2f'),
+        'Evolução 6M Relativa': st.column_config.NumberColumn('Evolução 6M Relativa', format='percent'),
+        'Evolução 12M Relativa': st.column_config.NumberColumn('Evolução 12M Relativa', format='percent'),
+        'Evolução 24M Relativa': st.column_config.NumberColumn('Evolução 24M Relativa', format='percent')
+    }
+    
+    with tab_stats:
+        st.dataframe(df_stats, column_config=columns_config)
+    
+    with tab_abs:
+        abs_col = [
+            'Diferença Mensal Abs.',
+            'Média 6M Diferença Mensal Abs.',
+            'Média 12M Diferença Mensal Abs.',
+            'Média 24M Diferença Mensal Abs.'
+        ]
+        
+        st.line_chart(df_stats[abs_col])
+    
+    with tab_rel:
+        rel_col = [
+            'Diferença Mensal Rel.',
+            'Evolução 6M Relativa',
+            'Evolução 12M Relativa',
+            'Evolução 24M Relativa'
+        ]
+        
+        st.line_chart(df_stats[rel_col])
+
 
 # Não tem arquivos...
